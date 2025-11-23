8b0be09785569659e7744be2018b84b7b40b1830
onyedkichi23 <ozohenrywk@gmail.com>
2025-11-23 17:13:37 +0100

    feat(layout): add SafeAreaView to the root layout.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,31 @@
-import '@/global.css';
+/** @format */
 
-import { NAV_THEME } from '@/lib/theme';
-import { ThemeProvider } from '@react-navigation/native';
-import { PortalHost } from '@rn-primitives/portal';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { useColorScheme } from 'nativewind';
+import "@/global.css";
+
+import { NAV_THEME } from "@/lib/theme";
+import { ThemeProvider } from "@react-navigation/native";
+import { PortalHost } from "@rn-primitives/portal";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "nativewind";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+	// Catch any errors thrown by the Layout component.
+	ErrorBoundary,
+} from "expo-router";
 
 export default function RootLayout() {
-  const { colorScheme } = useColorScheme();
+	const { colorScheme } = useColorScheme();
 
-  return (
-    <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
-      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Stack />
-      <PortalHost />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider value={NAV_THEME[colorScheme ?? "light"]}>
+			<StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+			<SafeAreaView className="flex-1 bg-background">
+				<Stack />
+			</SafeAreaView>
+
+			<PortalHost />
+		</ThemeProvider>
+	);
 }
