7102f5e877f80161488675fcc6563bb1d73fae16
onyedkichi23 <ozohenrywk@gmail.com>
2025-11-25 13:47:29 +0100

    feat(audio): integrate expo-audio for recording functionality and add RecordButton component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,12 +3,19 @@
 import { Button } from "@/components/ui/button";
 import { Icon } from "@/components/ui/icon";
 import { Text } from "@/components/ui/text";
+import { useEffectEvent } from "@/hooks/use-effect-event";
 import { cn } from "@/lib/utils";
 import { PulseCircle } from "@/screens/home";
+import {
+	AudioModule,
+	RecordingPresets,
+	setAudioModeAsync,
+	useAudioRecorder,
+} from "expo-audio";
 import { Link, Stack } from "expo-router";
-import { Mic, MoreVertical, Settings } from "lucide-react-native";
+import { Mic, MoreVertical, Settings, Square } from "lucide-react-native";
 import React from "react";
-import { FlatList, View } from "react-native";
+import { ActivityIndicator, Alert, FlatList, View } from "react-native";
 
 const DATA = [
 	{
@@ -48,6 +55,94 @@ const ModalButtonIcon = (props: React.ComponentProps<typeof Icon>) => (
 	<Icon size={32} {...props} />
 );
 
+const RecordButton = () => {
+	const [recordingState, setRecordingState] = React.useState<
+		"idle" | "starting" | "recording"
+	>("idle");
+
+	const audioRecorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY!);
+	// const recorderState = useAudioRecorderState(audioRecorder);
+
+	const record = async () => {
+		try {
+			setRecordingState("starting");
+			audioRecorder.record();
+			setRecordingState("recording");
+		} catch (error) {
+			Alert.alert(
+				"Error",
+				error instanceof Error ? error.message : String(error),
+			);
+			setRecordingState("idle");
+		}
+	};
+
+	const stopRecording = async () => {
+		// The recording will be available on `audioRecorder.uri`.
+		await audioRecorder.stop();
+		setRecordingState("idle");
+	};
+
+	const onInitialLoad = useEffectEvent(async () => {
+		try {
+			const [status] = await Promise.all([
+				AudioModule.requestRecordingPermissionsAsync(),
+				audioRecorder.prepareToRecordAsync(),
+			]);
+			if (!status.granted) {
+				throw new Error("Permission to access microphone was denied");
+			}
+			await setAudioModeAsync({
+				playsInSilentMode: true,
+				allowsRecording: true,
+			});
+		} catch (error) {
+			Alert.alert(
+				"Error",
+				error instanceof Error ? error.message : String(error),
+			);
+		}
+	});
+
+	React.useEffect(() => {
+		void onInitialLoad();
+	}, []);
+
+	return (
+		<Button
+			className="relative aspect-square h-fit self-center rounded-full bg-accent p-8 active:bg-accent/50"
+			onPress={() => {
+				if (recordingState === "idle") {
+					void record();
+					return;
+				}
+				if (recordingState === "recording") {
+					void stopRecording();
+				}
+			}}>
+			{recordingState === "idle" && <Icon as={Mic} size={40} />}
+
+			{recordingState === "starting" && (
+				<ActivityIndicator className="fill-foreground" size={40} />
+			)}
+
+			{recordingState === "recording" && (
+				<>
+					{Array.from({ length: 3 }).map((_, index) => (
+						<PulseCircle
+							// eslint-disable-next-line @eslint-react/no-array-index-key
+							key={index}
+							index={index}
+							className="bg-accent"
+						/>
+					))}
+					<Icon as={Square} className="fill-foreground" size={40} />
+				</>
+			)}
+		</Button>
+	);
+};
+
 export default function Home() {
 	return (
 		<>
@@ -78,18 +173,7 @@ export default function Home() {
 						</ModalButton>
 					</Link>
 
-					<Button className="relative aspect-square h-fit self-center rounded-full bg-accent p-8 active:bg-accent/50">
-						{Array.from({ length: 3 }).map((_, index) => (
-							<PulseCircle
-								// eslint-disable-next-line @eslint-react/no-array-index-key
-								key={index}
-								index={index}
-								className="bg-accent"
-							/>
-						))}
-
-						<Icon as={Mic} size={52} />
-					</Button>
+					<RecordButton />
 
 					<ModalButton>
 						<ModalButtonIcon as={MoreVertical} />
